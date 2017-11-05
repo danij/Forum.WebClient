70162fdac94b4d14639d035acaba5c8831f8bba0
Dani J <dani.user@gmail.com>
2017-11-05 21:37:30 +0200

    Set up webpack to copy included js and css files to dist/

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/app.ts',
@@ -10,6 +11,28 @@ module.exports = {
     devtool: 'source-map',
     plugins: [
         // new webpack.optimize.UglifyJsPlugin()
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, 'node_modules/jquery/dist/jquery.min.js'),
+                to: path.resolve(__dirname, 'dist/jquery.min.js')
+            },
+            {
+                from: path.resolve(__dirname, 'node_modules/uikit/dist/js/uikit.min.js'),
+                to: path.resolve(__dirname, 'dist/uikit.min.js')
+            },
+            {
+                from: path.resolve(__dirname, 'node_modules/uikit/dist/css/uikit.min.css'),
+                to: path.resolve(__dirname, 'dist/uikit.min.css')
+            },
+            {
+                from: path.resolve(__dirname, 'node_modules/uikit/dist/js/uikit-icons.min.js'),
+                to: path.resolve(__dirname, 'dist/uikit-icons.min.js')
+            },
+            {
+                from: path.resolve(__dirname, 'node_modules/es6-promise/dist/es6-promise.auto.min.js'),
+                to: path.resolve(__dirname, 'dist/es6-promise.auto.min.js')
+            }
+        ])
     ],
     resolve: {
         extensions: ['.webpack.js', '.web.js', '.ts', '.js']
