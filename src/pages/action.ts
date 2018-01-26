9ffb9497dca957531d16cc986180d50286a4fa4f
Dani J <dani.user@gmail.com>
2018-01-26 19:29:13 +0200

    Removed unused async/await pairs

--- src/pages/action.ts
+++ src/pages/action.ts
@@ -108,44 +108,44 @@ export module PageActions {
 
     class CategoryCallback implements ICategoryCallback {
 
-        async createRootCategory(name: string): Promise<boolean> {
+        createRootCategory(name: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(CategoryRepository.addNewCategory(name));
+            return Pages.trueOrShowErrorAndFalse(CategoryRepository.addNewCategory(name));
         }
 
-        async createSubCategory(parentId: string, name: string): Promise<boolean> {
+        createSubCategory(parentId: string, name: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(CategoryRepository.addNewCategory(name, parentId));
+            return Pages.trueOrShowErrorAndFalse(CategoryRepository.addNewCategory(name, parentId));
         }
 
-        async deleteCategory(id: string): Promise<boolean> {
+        deleteCategory(id: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(CategoryRepository.deleteCategory(id));
+            return Pages.trueOrShowErrorAndFalse(CategoryRepository.deleteCategory(id));
         }
 
-        async editCategoryName(id: string, newName: string): Promise<boolean> {
+        editCategoryName(id: string, newName: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(CategoryRepository.editCategoryName(id, newName));
+            return Pages.trueOrShowErrorAndFalse(CategoryRepository.editCategoryName(id, newName));
         }
 
-        async editCategoryDescription(id: string, newDescription: string): Promise<boolean> {
+        editCategoryDescription(id: string, newDescription: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(CategoryRepository.editCategoryDescription(id, newDescription));
+            return Pages.trueOrShowErrorAndFalse(CategoryRepository.editCategoryDescription(id, newDescription));
         }
 
-        async editCategoryDisplayOrder(id: string, newDisplayOrder: number): Promise<boolean> {
+        editCategoryDisplayOrder(id: string, newDisplayOrder: number): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(CategoryRepository.editCategoryDisplayOrder(id, newDisplayOrder));
+            return Pages.trueOrShowErrorAndFalse(CategoryRepository.editCategoryDisplayOrder(id, newDisplayOrder));
         }
 
-        async editCategoryParent(id: string, newParentId: string): Promise<boolean> {
+        editCategoryParent(id: string, newParentId: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(CategoryRepository.editCategoryParent(id, newParentId));
+            return Pages.trueOrShowErrorAndFalse(CategoryRepository.editCategoryParent(id, newParentId));
         }
 
-        async editCategoryTags(id: string, addTagIds: string[], removeTagIds: string[]): Promise<boolean> {
+        editCategoryTags(id: string, addTagIds: string[], removeTagIds: string[]): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(Promise.all(
+            return Pages.trueOrShowErrorAndFalse(Promise.all(
                 (addTagIds || []).map((tagId) => CategoryRepository.addTagToCategory(id, tagId))
                     .concat((removeTagIds || []).map((tagId) => CategoryRepository.removeTagFromCategory(id, tagId)))
             ));
@@ -159,24 +159,24 @@ export module PageActions {
 
     class TagCallback implements ITagCallback {
 
-        async createTag(name: string): Promise<boolean> {
+        createTag(name: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(TagRepository.addNewTag(name));
+            return Pages.trueOrShowErrorAndFalse(TagRepository.addNewTag(name));
         }
 
-        async deleteTag(id: string): Promise<boolean> {
+        deleteTag(id: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(TagRepository.deleteTag(id));
+            return Pages.trueOrShowErrorAndFalse(TagRepository.deleteTag(id));
         }
 
-        async editTagName(id: string, newName: string): Promise<boolean> {
+        editTagName(id: string, newName: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(TagRepository.editTagName(id, newName));
+            return Pages.trueOrShowErrorAndFalse(TagRepository.editTagName(id, newName));
         }
 
-        async mergeTags(sourceId: string, destinationId: string): Promise<boolean> {
+        mergeTags(sourceId: string, destinationId: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(TagRepository.mergeTags(sourceId, destinationId));
+            return Pages.trueOrShowErrorAndFalse(TagRepository.mergeTags(sourceId, destinationId));
         }
 
         getAllTags(): Promise<TagRepository.Tag[]> {
@@ -198,42 +198,42 @@ export module PageActions {
             return result;
         }
 
-        async deleteThread(id: string): Promise<boolean> {
+        deleteThread(id: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(ThreadRepository.deleteThread(id));
+            return Pages.trueOrShowErrorAndFalse(ThreadRepository.deleteThread(id));
         }
 
-        async editThreadName(id: string, newName: string): Promise<boolean> {
+        editThreadName(id: string, newName: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(ThreadRepository.editThreadName(id, newName));
+            return Pages.trueOrShowErrorAndFalse(ThreadRepository.editThreadName(id, newName));
         }
 
-        async editThreadPinDisplayOrder(id: string, newPinDisplayOrder: number): Promise<boolean> {
+        editThreadPinDisplayOrder(id: string, newPinDisplayOrder: number): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(ThreadRepository.editThreadPinDisplayOrder(id, newPinDisplayOrder));
+            return Pages.trueOrShowErrorAndFalse(ThreadRepository.editThreadPinDisplayOrder(id, newPinDisplayOrder));
         }
 
-        async editThreadTags(id: string, addTagIds: string[], removeTagIds: string[]): Promise<boolean> {
+        editThreadTags(id: string, addTagIds: string[], removeTagIds: string[]): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(Promise.all(
+            return Pages.trueOrShowErrorAndFalse(Promise.all(
                 (addTagIds || []).map((tagId) => ThreadRepository.addTagToThread(id, tagId))
                     .concat((removeTagIds || []).map((tagId) => ThreadRepository.removeTagFromThread(id, tagId)))
             ));
         }
 
-        async subscribeToThread(id: string): Promise<boolean> {
+        subscribeToThread(id: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(ThreadRepository.subscribeToThread(id));
+            return Pages.trueOrShowErrorAndFalse(ThreadRepository.subscribeToThread(id));
         }
 
-        async unSubscribeFromThread(id: string): Promise<boolean> {
+        unSubscribeFromThread(id: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(ThreadRepository.unSubscribeFromThread(id));
+            return Pages.trueOrShowErrorAndFalse(ThreadRepository.unSubscribeFromThread(id));
         }
 
-        async mergeThreads(sourceId: string, destinationId: string): Promise<boolean> {
+        mergeThreads(sourceId: string, destinationId: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(ThreadRepository.mergeThreads(sourceId, destinationId));
+            return Pages.trueOrShowErrorAndFalse(ThreadRepository.mergeThreads(sourceId, destinationId));
         }
 
         searchThreadsByInitial(name: string): Promise<ThreadRepository.Thread[]> {
@@ -241,7 +241,7 @@ export module PageActions {
             return Pages.getOrShowErrorAndDefault(ThreadRepository.searchThreadsByInitial(name), () => []);
         }
 
-        async searchThreadsByName(name: string): Promise<ThreadRepository.Thread[]> {
+        searchThreadsByName(name: string): Promise<ThreadRepository.Thread[]> {
 
             return Pages.getOrShowErrorAndDefault(ThreadRepository.searchThreadsByName(name), () => []);
         }
@@ -251,9 +251,9 @@ export module PageActions {
             return UserRepository.getUsersSubscribedToThread(id);
         }
 
-        async addThreadMessage(id: string, content: string): Promise<string> {
+        addThreadMessage(id: string, content: string): Promise<string> {
 
-            return await Pages.getOrShowError(ThreadMessageRepository.addThreadMessage(id, content.trim()));
+            return Pages.getOrShowError(ThreadMessageRepository.addThreadMessage(id, content.trim()));
         }
 
         getAllTags(): Promise<TagRepository.Tag[]> {
@@ -264,30 +264,30 @@ export module PageActions {
 
     class ThreadMessageCallback implements IThreadMessageCallback {
 
-        async editThreadMessageContent(id: string, newContent: string, changeReason: string): Promise<boolean> {
+        editThreadMessageContent(id: string, newContent: string, changeReason: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(
+            return Pages.trueOrShowErrorAndFalse(
                 ThreadMessageRepository.editThreadMessageContent(id, newContent.trim(), changeReason.trim()));
         }
 
-        async moveThreadMessage(id: string, targetThreadId: string): Promise<boolean> {
+        moveThreadMessage(id: string, targetThreadId: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(ThreadMessageRepository.moveThreadMessage(id, targetThreadId));
+            return Pages.trueOrShowErrorAndFalse(ThreadMessageRepository.moveThreadMessage(id, targetThreadId));
         }
 
-        async deleteThreadMessage(id: string): Promise<boolean> {
+        deleteThreadMessage(id: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(ThreadMessageRepository.deleteThreadMessage(id));
+            return Pages.trueOrShowErrorAndFalse(ThreadMessageRepository.deleteThreadMessage(id));
         }
 
-        async commentThreadMessage(id: string, comment: string): Promise<boolean> {
+        commentThreadMessage(id: string, comment: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(ThreadMessageRepository.commentThreadMessage(id, comment.trim()));
+            return Pages.trueOrShowErrorAndFalse(ThreadMessageRepository.commentThreadMessage(id, comment.trim()));
         }
 
-        async solveThreadMessageComment(id: string): Promise<boolean> {
+        solveThreadMessageComment(id: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(ThreadMessageRepository.solveCommentOfThreadMessage(id));
+            return Pages.trueOrShowErrorAndFalse(ThreadMessageRepository.solveCommentOfThreadMessage(id));
         }
 
         async getCommentsOfThreadMessage(id: string): Promise<ThreadMessageRepository.ThreadMessageComment[]> {
@@ -304,39 +304,39 @@ export module PageActions {
 
     class UserCallback implements IUserCallback {
 
-        async editUserName(id: string, newName: string): Promise<boolean> {
+        editUserName(id: string, newName: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(UserRepository.editUserName(id, newName.trim()));
+            return Pages.trueOrShowErrorAndFalse(UserRepository.editUserName(id, newName.trim()));
         }
 
-        async editUserInfo(id: string, newInfo: string): Promise<boolean> {
+        editUserInfo(id: string, newInfo: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(UserRepository.editUserInfo(id, newInfo.trim()));
+            return Pages.trueOrShowErrorAndFalse(UserRepository.editUserInfo(id, newInfo.trim()));
         }
 
-        async editUserTitle(id: string, newTitle: string): Promise<boolean> {
+        editUserTitle(id: string, newTitle: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(UserRepository.editUserTitle(id, newTitle.trim()));
+            return Pages.trueOrShowErrorAndFalse(UserRepository.editUserTitle(id, newTitle.trim()));
         }
 
-        async editUserSignature(id: string, newSignature: string): Promise<boolean> {
+        editUserSignature(id: string, newSignature: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(UserRepository.editUserSignature(id, newSignature.trim()));
+            return Pages.trueOrShowErrorAndFalse(UserRepository.editUserSignature(id, newSignature.trim()));
         }
 
-        async deleteUserLogo(id: string): Promise<boolean> {
+        deleteUserLogo(id: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(UserRepository.deleteUserLogo(id));
+            return Pages.trueOrShowErrorAndFalse(UserRepository.deleteUserLogo(id));
         }
 
-        async uploadUserLogo(id: string, fileContent: ArrayBuffer): Promise<boolean> {
+        uploadUserLogo(id: string, fileContent: ArrayBuffer): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(UserRepository.editUserLogo(id, fileContent));
+            return Pages.trueOrShowErrorAndFalse(UserRepository.editUserLogo(id, fileContent));
         }
 
-        async deleteUser(id: string): Promise<boolean> {
+        deleteUser(id: string): Promise<boolean> {
 
-            return await Pages.trueOrShowErrorAndFalse(UserRepository.deleteUser(id));
+            return Pages.trueOrShowErrorAndFalse(UserRepository.deleteUser(id));
         }
 
         searchUsersByName(name: string): Promise<UserRepository.User[]> {
