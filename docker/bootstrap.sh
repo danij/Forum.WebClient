3f2d975d064157b31817b4282c9603dc555cec7c
Dani J <dani.user@gmail.com>
2019-03-05 20:45:26 +0200

    Added bootstrap script for containers

---
+++ docker/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+HOSTNAME="$1"
+
+mkdir /forum/config/Forum.WebClient
+mkdir /forum/config/Forum.WebClient/doc
+
+cp /var/www/html/config/config.js /forum/config/Forum.WebClient/config.js
+
+sed -i 's#"google"#//"google"#' /forum/config/Forum.WebClient/config.js
+sed -i "s#http://dani.forum:8080#https://$HOSTNAME#" /forum/config/Forum.WebClient/config.js
+
+rm /var/www/html/config/config.js
+ln -s /forum/config/Forum.WebClient/config.js /var/www/html/config/config.js
+
+cp -a /var/www/html/doc/. /forum/config/Forum.WebClient/doc
+rm -r /var/www/html/doc
+ln -s /forum/config/Forum.WebClient/doc /var/www/html/doc
